5951f953ecb09cac4758b6ca1789268592523c9f
Joseph Naval <josephnaval47@gmail.com>
2023-08-28 22:22:07 +0800

    message networking

--- src/instance/sampleQuestion.jsx
+++ src/instance/sampleQuestion.jsx
@@ -6,30 +6,60 @@ const sample_questions =
             choices: ['Object-Oriented', 'Object-Based', 'Procedural', 'None of the Above'],
             answer: 'Object-Oriented'
         },
-  
-        // {
-        //     question: "Which of the following keywords is used to define a variable in Javascript?",
-        //     choices: ['var', 'let', 'Both var and let', 'None of the Above'],
-        //     answer: 'Both var and let'
-        // },
-  
-        // {
-        //     question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-        //     choices: ['Throws an error', 'Ignores the statements', 'Gives a warning', 'None of the Above'],
-        //     answer: 'Ignores the statements'
-        // },
 
-        // {
-        //     question: "How can a datatype be declared to be a constant type?",
-        //     choices: ['const', 'var', 'let', 'constant'],
-        //     answer: 'const'
-        // },
+        {
+            question: "Which of the following keywords is used to define a variable in Javascript?",
+            choices: ['var', 'let', 'Both var and let', 'None of the Above'],
+            answer: 'Both var and let'
+        },
+  
+        {
+            question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+            choices: ['Throws an error', 'Ignores the statements', 'Gives a warning', 'None of the Above'],
+            answer: 'Ignores the statements'
+        },
+
+        {
+            question: "How can a datatype be declared to be a constant type?",
+            choices: ['const', 'var', 'let', 'constant'],
+            answer: 'const'
+        },
  
-        // {
-        //     question: "What keyword is used to check whether a given property is valid or not?",
-        //     choices: ['in', 'is in', 'exists', 'lies'],
-        //     answer: 'in'
-        // },
+        {
+            question: "What keyword is used to check whether a given property is valid or not?",
+            choices: ['in', 'is in', 'exists', 'lies'],
+            answer: 'in'
+        },
+        {
+            question: "Which statement cannot be used to declare a variable in JavaScript?",
+            choices: ['let', 'var', 'int', 'const'],
+            answer: 'int'
+        },
+
+        {
+            question: "Which of the following is an array method?",
+            choices: ['map', 'filter', 'reduce', 'all of these'],
+            answer: 'all of these'
+        },
+
+        {
+            question: "Which of the following is the right way to add a single line comment in JavaScript?",
+            choices: ['#this is a comment', '##this is a comment', '/this is a comment', '//this is a comment'],
+            answer: '//this is a comment'
+        },
+
+        {
+            question: "What will ‘0 == false ’ evaluate to?",
+            choices: ['null', 'undefined', 'throws error', 'true'],
+            answer: 'true'
+        },
+
+        {
+            question: "What is the full form of DOM?",
+            choices: ['data object model', 'document or model', 'document object model', 'document object medium'],
+            answer: 'document object model'
+        },
+
         {
             question: "",
             choices: [],
--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -86,7 +86,7 @@ const UserDashboard = () => {
                             <img src="https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=996&t=st=1692953226~exp=1692953826~hmac=6c964fd820b0a896379598dcfe5d0ce615aaae65a6a044226e1e4836699ce260" className="card-img-top" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title fw-medium">Javascript quiz</h5>
-                                <p className="card-text">10 questions, all about javascript and its fundamentals</p>
+                                <p className="card-text">all about javascript and its fundamentals</p>
                                 <div role='button' onClick={createRoom} className="text-white th-bg-main px-3 py-2 text-center rounded-3">Create Room</div>
                             </div>
                         </div>   
